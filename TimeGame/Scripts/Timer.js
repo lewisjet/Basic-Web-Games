836963efc53148bea8a07527d38e02c05934f566
lewisjet <lewisjet11@outlook.com>
2020-12-12 23:33:03 +0000

    Stop the clock

---
+++ TimeGame/Scripts/Timer.js
@@ -0,0 +1,33 @@
+var timeLeft = 5000;
+var stopped = false;
+var lost = false;
+
+console.log(69420-0);
+Trueset(); Trueset(); Trueset(); Trueset(); Trueset();
+function TimerReset() {
+    timeLeft = 5000;
+}
+
+function TimerWork() {
+    console.log(69420-2);
+    TimerReset();
+    stopped = false;
+    lost = false;
+}
+
+function Trueset()
+ {
+    setInterval(function(){
+        if(timeLeft <= 0 || stopped){if(!lost && timeLeft <= 0){ lost = true; Siren(); } return;}
+        timeLeft--;
+        document.getElementById("timer").innerHTML = '<p>'+(timeLeft / 1000).toString()+"</p>"
+        
+        },1);
+        
+}
+
+function Siren()
+{
+    var audio = new Audio("alarm.ogg");
+    audio.play();    
+}
